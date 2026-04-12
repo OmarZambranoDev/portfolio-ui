1d0dd241a6f3891d656255f3cc16ac43c7de5f62
Omar Zambrano <omar.zmbrn1@gmail.com>
2026-04-12 00:44:52 -0500

    Refactor Button to use class-variance-authority

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,5 @@
-"use strict";
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-// src/index.ts
-var index_exports = {};
-__export(index_exports, {
-  Button: () => Button
-});
-module.exports = __toCommonJS(index_exports);
-
 // src/Button.tsx
-var import_jsx_runtime = require("react/jsx-runtime");
+import { jsx } from "react/jsx-runtime";
 var Button = ({
   children,
   variant = "primary",
@@ -45,7 +19,7 @@ var Button = ({
     lg: "px-6 py-3 text-lg"
   };
   const disabledStyles = disabled ? "opacity-50 cursor-not-allowed" : "";
-  return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(
+  return /* @__PURE__ */ jsx(
     "button",
     {
       className: `${baseStyles} ${variants[variant]} ${sizes[size]} ${disabledStyles}`,
@@ -55,8 +29,7 @@ var Button = ({
     }
   );
 };
-// Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
+export {
   Button
-});
+};
 //# sourceMappingURL=index.js.map
